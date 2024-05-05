9bb9a8266691aafd764c15c981a06127cf15eb2d
Jitendra <info.jiitendra@gmail.com>
2024-05-05 11:30:02 +0530

    Initial commit

--- src/routes/layout/Layout.jsx
+++ src/routes/layout/Layout.jsx
@@ -1,10 +1,12 @@
 import Navbar from "../../components/navbar/Navbar";
+import HomePage from "../homePage/HomePage";
 import "./layout.scss";
 
 function Layout() {
   return (
     <div className="layout">
       <Navbar />
+      <HomePage />
     </div>
   );
 }
